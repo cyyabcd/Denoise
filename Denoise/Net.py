ea5c8ebaed9c07433392850604bccbb614af560a
DESKTOP-RRHJ3AV\chen <603507745@qq.com>
2018-02-06 16:56:09 -0500

    12
    net

--- Denoise/Net.py
+++ Denoise/Net.py
@@ -2,7 +2,8 @@ import numpy as np
 import tensorflow as tf
 
 # RecNet Model
-n_channel = 3
+
+n_channels = [3,32]
 def RecNet(_X,J):
     out = RecNet_level(_X, J, 1)
     return out
@@ -19,53 +20,53 @@ def RecNet_level(_X, J, l):
     if l == J:
         out = tf.layers.conv2d(
             inputs = _X,
-            filters = n_channel,
+            filters = n_channels[J-1],
             kernel_size = [5,5],
             padding = "same",
             activation = tf.nn.relu)
     else:
         conv1 = tf.layers.conv2d(
             inputs = _X,
-            filters = n_channel,
+            filters = n_channels[l-1],
             kernel_size = [5,5],
             padding = "same",
             activation = tf.nn.relu)
-        conv2 = _X + tf.layers.conv2d(
+        conv2 = conv1 + tf.layers.conv2d(
             inputs = conv1,
-            filters = n_channel,
+            filters = n_channels[l-1],
             kernel_size = [5,5],
             padding = "same",
             activation = tf.nn.relu)
         resconv = _X - tf.layers.conv2d(
             inputs = conv2,
-            filters = n_channel,
+            filters = n_channels[l-1],
             kernel_size = [5,5],
             padding = "same",
             activation = tf.nn.relu);
         xc= tf.layers.conv2d(
             inputs = resconv,
-            filters = n_channel,
+            filters = n_channels[l],
             kernel_size = [5,5],
             strides = [2,2],
             padding = "same",
             activation = tf.nn.relu)
         ec = RecNet_level(xc,J,l+1)
-        convtp1 = conv1 + tf.layers.conv2d_transpose(
+        convtp1 = conv2 + tf.layers.conv2d_transpose(
             inputs = ec,
-            filters = n_channel,
+            filters = n_channels[l-1],
             kernel_size = [5,5],
             strides = [2,2],
             padding = "same",
             activation = tf.nn.relu)
-        out1 = convtp1 + tf.layers.conv2d(
+        out1 = convtp1 + tf.layers.conv2d_transpose(
             inputs = convtp1,
-            filters = n_channel,
+            filters = n_channels[l-1],
             kernel_size = [5,5],
             padding = "same",
             activation = tf.nn.relu)
-        out =  tf.layers.conv2d(
+        out =  tf.layers.conv2d_transpose(
             inputs = out1,
-            filters = n_channel,
+            filters = n_channels[l-1],
             kernel_size = [5,5],
             padding = "same",
             activation = tf.nn.relu)
